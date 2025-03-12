d2c1d0977f88522b1b150f85966e1e6b545cfe1a
redacted <redacted>
2025-03-12 23:15:23 +0100

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,14 @@
 
 ((nil
   .
-  ((eval . (add-to-list 'completion-ignored-extensions ".go"))))
+  ((fill-column . 78)
+   (tab-width   .  8)
+   (sentence-end-double-space . t)
+   (whitespace-cleanup-mode-only-if-initially-clean)
+   (eval . (add-to-list 'completion-ignored-extensions ".go"))))
+ (prog-mode
+  .
+  ((mode . whitespace-cleanup)))
  (scheme-mode
   .
   ;; This is a temporary setting until dependency on guix is removed
