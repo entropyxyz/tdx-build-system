85af90366d918173751e35e598a1b09808cca834
redacted <redacted>
2025-03-13 03:59:36 +0100

    Update .guix-channel. Add nonguix.

--- channels.scm
+++ channels.scm
@@ -1,10 +1,21 @@
 (list
  (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
+  (branch "master")
+  (commit
+   "944619c1947ba59823f4ad6d8b13842b7e162d76")
+  (introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+ (channel
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
   (branch "master")
   (commit
-   "9b98e4b0ee45fb712d815601645fb561a83b4e25")
+   "963213f2510698f25f44f863a35c76dc108aa6b3")
   (introduction
    (make-channel-introduction
     "9edb3f66fd807b096b48283debdcddccfea34bad"
